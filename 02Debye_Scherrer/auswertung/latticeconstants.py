e600ef8b8b45c315b98f7f2af0024dd36da9cf68
Diman1992 <d.skodras@gmx.de>
2015-09-16 13:57:41 +0200

    debye  Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

--- 02Debye_Scherrer/auswertung/latticeconstants.py
+++ 02Debye_Scherrer/auswertung/latticeconstants.py
@@ -10,15 +10,15 @@ def sortArrayUpways(arr):
 				arr[i] = temp
 
 lattice = list()
-indextriple = 6
+indextriple = 0
 maxindex = 7
 
 for h in range(0,maxindex+1):
 	for k in range(0,maxindex+1):
 		for l in range(0,maxindex+1):
 			lattice.append(h**2+k**2+l**2)
-			if(h**2+k**2+l**2==indextriple):
-				print(h,k,l,h**2+k**2+l**2)
+			#if(h**2+k**2+l**2==indextriple):
+			print(h,k,l,h**2+k**2+l**2)
 
 sortArrayUpways(lattice)
 #print(lattice)
