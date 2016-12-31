2ccc69445855c589c5f429773cce755f1c7d56d3
Diman1992 <d.skodras@gmx.de>
2016-12-31 03:25:26 +0100

    myonen auswertung kalib, koinz, TODO tau

---
+++ 07Myonen/auswertung/script.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from uncertainties import ufloat, unumpy
+from uncertainties.umath import exp
