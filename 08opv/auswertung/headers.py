b5bac7878dad7eb8e416a3dc5c1be8fa573a1922
jan <jlatarius@web.de>
2017-01-15 14:16:32 +0100

    opv auswertung angefangen

---
+++ 08opv/auswertung/headers.py
@@ -0,0 +1,12 @@
+import fileinput
+import sys
+import glob
+
+for file in glob.glob('./data/scope_*.csv'):
+	i = 0
+	for line in fileinput.input(file, inplace=True):
+		if(i < 2):
+			sys.stdout.write('#{l}'.format(l=line))
+			i = i + 1
+		else:
+			sys.stdout.write('{l}'.format(l=line))
